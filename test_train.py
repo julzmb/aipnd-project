247359ca56c3f918ef51b6bd66b951364fab6998
Julian Mullings-Black <julzmb@gmail.com>
2019-10-03 11:06:27 +0000

    unittest: getargs finised tests

--- test_train.py
+++ test_train.py
@@ -32,11 +32,6 @@ class MainTests(unittest.TestCase):
 
 class GetArgsTests(unittest.TestCase):
     @unittest.mock.patch('argparse.ArgumentParser')
-    def test_getargs_instantiates_parser(self, mock_class):
-        train.getargs()
-        mock_class.assert_called()
-
-    @unittest.mock.patch('argparse.ArgumentParser')
     def test_getargs_return_argument_parser(self, mock_class):
         test_parser = unittest.mock.MagicMock()
         mock_class.return_value = test_parser
@@ -45,16 +40,144 @@ class GetArgsTests(unittest.TestCase):
             test_parser
         )
 
+    def get_args(
+        self,
+        data_dir='test_data_dir',
+        save_dir='test_save_dir',
+        arch='vgg16',
+        learning_rate='0',
+        hidden_units=['0'],
+        epochs='0',
+        gpu=None
+    ):
+        args = []
+        if data_dir is not None:
+            args += [data_dir]
+
+        if save_dir is not None:
+            args += ['--save_dir', save_dir]
+
+        if arch is not None:
+            args += ['--arch', arch]
+
+        if learning_rate is not None:
+            args += ['--learning_rate', learning_rate]
+
+        if hidden_units is not None:
+            args += ['--hidden_units']
+            args += hidden_units
+
+        if epochs is not None:
+            args += ['--epochs', epochs]
+
+        if gpu:
+            args+= ['--gpu']
+
+        return train.getargs().parse_args(args)
+
     def test_getargs_positional_args_datadir(self):
-        test_datadir_value = 'data/version1/'
-        args = train.getargs().parse_args(
-            [test_datadir_value]
+        test_data_dir = 'data/version1/'
+        args = self.get_args(
+            data_dir=test_data_dir
         )
         self.assertEqual(
             args.data_dir,
-            test_datadir_value
+            test_data_dir
         )
 
+    def test_getargs_savedir_arg(self):
+        test_savedir = 'model_output'
+        args = self.get_args(
+            save_dir=test_savedir
+        )
+        self.assertEqual(
+            args.save_dir,
+            test_savedir
+        )
+
+    def test_getargs_savedir_not_included(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(
+                save_dir=None
+            )
+
+    def test_getargs_arch_arg(self):
+        test_architecture = 'vgg13'
+        args = self.get_args(
+            arch=test_architecture
+        )
+        self.assertEqual(
+            args.arch,
+            test_architecture
+        )
+
+    def test_getargs_arch_not_included(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(
+                arch=None
+            )
+
+    def test_getargs_arch_not_in_choices(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(arch='JulianModelArch')
+
+    def test_getargs_learning_rate_arg(self):
+        test_learning_rate = '0.0001'
+        args = self.get_args(
+            learning_rate=test_learning_rate
+        )
+        self.assertEqual(
+            args.learning_rate,
+            float(test_learning_rate)
+        )
+
+    def test_getargs_learning_rate_not_included(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(learning_rate=None)
+
+    def run_hidden_units_test(self, test_hidden_units):
+        args = self.get_args(
+            hidden_units=test_hidden_units
+        )
+        expected_hidden_units = [int(x) for x in test_hidden_units]
+        self.assertEqual(
+            args.hidden_units,
+            expected_hidden_units
+        )
+
+    def test_getargs_hidden_units_arg(self):
+        self.run_hidden_units_test(['128'])
+    
+    def test_getargs_hidden_units_arg_multivalue(self):
+        self.run_hidden_units_test(['128', '64'])
+
+    def test_getargs_hidden_units_not_included(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(hidden_units=None)
+
+    def test_getargs_epochs_arg(self):
+        test_epochs = '10'
+        args = self.get_args(
+            epochs=test_epochs
+        )
+        self.assertEqual(
+            args.epochs,
+            int(test_epochs)
+        )
+
+    def test_gerargs_epochs_not_included(self):
+        with self.assertRaises(SystemExit):
+            self.get_args(epochs=None)
+
+    def test_getargs_gpu_arg(self):
+        args = self.get_args(
+            gpu=True
+        )
+        self.assertTrue(args.gpu)
+
+    def test_getargs_gpu_not_included(self):
+        args = self.get_args()
+        self.assertFalse(args.gpu)
 
 class GetDataLoadersTests(unittest.TestCase):
     pass
--- train.py
+++ train.py
@@ -25,9 +25,29 @@ def main():
 '''
 def getargs():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'data_dir'
-    )
+    parser.add_argument('data_dir')
+    parser.add_argument('--save_dir', required=True)
+    choices = [
+        'alexnet',
+        'densenet121', 'densenet161', 'densenet169', 'densenet201',
+        'googlenet',
+        'inception_v3',
+        'mnasnet0_5', 'mnasnet0_75', 'mnasnet1_0', 'mnasnet1_3',
+        'mobilenet_v2',
+        'resnet101', 'resnet152', 'resnet18', 'resnet34', 'resnet50',
+        'resnext101_32x8d', 'resnext50_32x4d',
+        'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0',
+        'shufflenet_v2_x1_5', 'shufflenet_v2_x2_0',
+        'squeezenet1_0', 'squeezenet1_1',
+        'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
+        'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn',
+        'wide_resnet101_2', 'wide_resnet50_2'
+    ]
+    parser.add_argument('--arch', required=True, choices=choices)
+    parser.add_argument('--learning_rate', type=float, required=True)
+    parser.add_argument('--hidden_units', type=int, nargs='+', required=True)
+    parser.add_argument('--epochs', type=int, required=True)
+    parser.add_argument('--gpu', action='store_true')
     return parser
 
 '''
